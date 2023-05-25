84923891e3fc4ba8fb11812360016098be0c6a54
Emanuel Valec <emanuel.valec@skole.hr>
2023-05-26 00:03:17 +0200

    Uredi formu za prijavu

--- Software/CleaningSolutions/CleaningSolutions/FrmLogin.cs
+++ Software/CleaningSolutions/CleaningSolutions/FrmLogin.cs
@@ -13,5 +13,9 @@ namespace CleaningSolutions {
         public FrmLogin() {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
     }
 }
