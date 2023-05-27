664eb21f32c7d77423a2d85eb88843616167a10b
Emanuel Valec <emanuel.valec@skole.hr>
2023-05-27 10:46:23 +0200

    Kreiraj WorkerRepository

---
+++ Software/CleaningSolutions/CleaningSolutions/Models/WorkerRepository.cs
@@ -0,0 +1,70 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CleaningSolutions.Models
+
+{
+    public class WorkerRepository
+    {
+        public static Worker GetWorker(int id)
+        {
+            Worker worker = null;
+
+            string sql = $"SELECT * FROM Workers WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                worker = CreateObject(reader);
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return worker;
+        }
+
+        public static List<Worker> GetWorkers()
+        {
+            var Workers = new List<Worker>();
+
+            string sql = "SELECT * FROM Workers";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Worker worker = CreateObject(reader);
+                Workers.Add(worker);
+            }
+
+            reader.Close();
+            DB.CloseConnection();
+
+            return Workers;
+        }
+
+        private static Worker CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string firstName = reader["FirstName"].ToString();
+            string lastName = reader["LastName"].ToString();
+            string phoneNumber = reader["PhoneNumber"].ToString();
+
+            var worker = new Worker
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                PhoneNumber = phoneNumber
+            };
+
+            return worker;
+        }
+    }
+}
